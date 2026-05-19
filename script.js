c3d09bb5ef361dcf326d7da92886fef9d2d5d391
seungpyo-jiyeon <wldus1452@gmail.com>
2026-05-19 20:54:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -336,15 +336,13 @@
        <span class="parent-father">${g.father}</span>
        <span class="parent-dot">•</span>
        <span class="parent-mother">${g.mother}</span>
-       <span class="parent-relation">의 아들</span>
-       <span class="child-name">승표</span>
+       <span class="parent-relation">의 아들 승표</span>
      </div>
       <div class="parent-row">
       <span class="parent-father">${b.father}</span>
       <span class="parent-dot">•</span>
       <span class="parent-mother">${b.mother}</span>
-      <span class="parent-relation">의&nbsp;&nbsp;&nbsp;딸</span>
-      <span class="child-name">지연</span>
+      <span class="parent-relation">의 딸 지연</span>
       </div>
     `;
 
